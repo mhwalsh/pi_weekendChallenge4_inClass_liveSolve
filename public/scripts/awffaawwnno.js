ccf9f8905b415d90119b0afaa5d40ab77a211005
devjanaprime <dev@primeacademy.io>
2016-09-19 14:19:30 -0500

    showTasks

--- public/scripts/awffaawwnno.js
+++ public/scripts/awffaawwnno.js
@@ -2,4 +2,10 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
+  showTasks();
 }); // end doc ready
+
+// show all tasks
+var showTasks = function(){
+    console.log( 'in showTasks');
+} // end showTasks
