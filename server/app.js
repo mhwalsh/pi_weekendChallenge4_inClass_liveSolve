166024ddd3a03f45772d0fb2d7f8b5dbf8b5d4f0
devjanaprime <dev@primeacademy.io>
2016-09-19 13:52:44 -0500

    first

---
+++ server/app.js
@@ -0,0 +1,28 @@
+var express = require( 'express' );
+var app = express();
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var pg = require( 'pg' );
+var connectionString = 'postgres://localhost:5432/todo';
+var port = process.env.PORT || 8027;
+
+// spin up server
+app.listen( port, function(){
+  console.log( 'server up on ' + port );
+}); //end server up
+
+// base
+app.get( '/', function( req, res ){
+  console.log( 'base url hit' );
+  res.sendFile( path.resolve( 'public/index.html' ) );
+});
+
+// get all tasks
+
+// put update a task to complete
+
+// post to create a new task
+
+// static folder
+app.use( express.static( 'public' ) );
